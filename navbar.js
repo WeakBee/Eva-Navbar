79490645e3f3f83dac66f5ff44116da8c71c25ef
Ichwanul Muslim <82445431+WeakBee@users.noreply.github.com>
2022-04-05 10:22:17 +0700

    update responsive

--- navbar.js
+++ navbar.js
@@ -3,29 +3,30 @@ $(document).ready(function () {
 
   $win.scroll(function () {
     if ($win.scrollTop() > 0) {
-      $("#nav .container").addClass("scrolltop");
+      $("#nav .container-lg").addClass("scrolltop");
     } else {
-      $("#nav .container").removeClass("scrolltop");
+      $("#nav .container-lg").removeClass("scrolltop");
     }
   });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  autohide = document.querySelector(".navbar");
+  el_autohide = document.querySelector("#nav .navbar");
 
-  navbar_height = document.querySelector(".navbar").offsetHeight;
+  // add padding-top to bady (if necessary)
+  navbar_height = document.querySelector("#nav .navbar").offsetHeight;
 
-  if (autohide) {
+  if (el_autohide) {
     var last_scroll_top = 0;
     window.addEventListener("scroll", function () {
       let scroll_top = window.scrollY;
       if (scroll_top < last_scroll_top) {
-        autohide.classList.remove("scrolled-down");
-        autohide.classList.add("scrolled-up");
+        el_autohide.classList.remove("scrolled-down");
+        el_autohide.classList.add("scrolled-up");
       } else if (scroll_top < 600) {
       } else {
-        autohide.classList.remove("scrolled-up");
-        autohide.classList.add("scrolled-down");
+        el_autohide.classList.remove("scrolled-up");
+        el_autohide.classList.add("scrolled-down");
       }
       last_scroll_top = scroll_top;
     });
@@ -34,3 +35,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // if
 });
 // DOMContentLoaded  end
+
+document.querySelector('.first-button').addEventListener('click', function () {
+
+  document.querySelector('.animated-icon1').classList.toggle('open');
+  });
